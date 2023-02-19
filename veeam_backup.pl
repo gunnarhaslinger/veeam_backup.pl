b5c8bb3ce7442e445a188ecc12cb8dd2a87a43e2
Gunnar Haslinger <gh.github@hitco.at>
2023-02-19 19:43:49 +0100

    Update for Veeam  6.0.0

--- veeam_backup.pl
+++ veeam_backup.pl
@@ -15,17 +15,17 @@ my $starttime = time;
 
 # Get Job-ID of configured Veeam Backup Job, in Free-Edition there can only be one Job existent
 $out = `$veeamconfig job list`; $job_id = $1 if ($out =~ /(\{[\w\-]+\})/m);
-if (!$job_id) { print STDERR "ERROR: Command \"job list\" didn't provide a configured Job-ID!\n"; exit 10; }
+if (!$job_id) { print STDERR "ERROR: Command \"job list\" didn't provide a configured Job-ID!\n\n$out\n\n"; exit 10; }
 
 # Get Job-Info and extract Job-Name
 $out = `$veeamconfig job info --id $job_id`; $job_name = $1 if ($out =~ /Name: (\S+)/m);
-if (!$job_name) { print STDERR "ERROR: Command \"job info\" didn't provide the Job-Name!\n"; exit 20; }
+if (!$job_name) { print STDERR "ERROR: Command \"job info\" didn't provide the Job-Name!\n\n$out\n\n"; exit 20; }
 
 # Start the Veeam Backup Job and get the Session-ID
 print timestamp() . " - Starting Veeam Backup-Job Name: $job_name (ID: $job_id) ...\n";
-$out = `$veeamconfig job start --id $job_id --retriable --scheduled --highpriority`;
-$session_id = $1 if ($out =~ /Session ID:\s+\[(\{[\w\-]+\})\]/im);
-if (!$session_id) { print STDERR "ERROR: Command \"job start\" didn't provide a Session-ID!\n"; exit 30; }
+$out = `$veeamconfig job start --id $job_id`;
+$session_id = $1 if ($out =~ /^Session ID:\s+\[(\{[\w\-]+\})\]/im);
+if (!$session_id) { print STDERR "ERROR: Command \"job start\" didn't provide a Session-ID!\n\n$out\n\n"; exit 30; }
 
 # Wait for Job finishing
 do { sleep($sleeptime);
